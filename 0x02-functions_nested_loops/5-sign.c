da67ba1f5fce6b350b415e0250f2e413c0ee52e1
Simeon-Charity Ojo <83595680+Simeon-Charity@users.noreply.github.com>
2022-09-15 06:42:49 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,8 @@
  * print_sign - print the sign of a number
  * @n: The character to be checked.
  *
- * Return: 1 and print + if n is greater than 0, return 0 if n is 0, return -1 is n is -
+ * Return: 1 and print + if n is greater than 0, 
+ * return 0 if n is 0, return -1 is n is -
  */
 int print_sign(int n)
 {
