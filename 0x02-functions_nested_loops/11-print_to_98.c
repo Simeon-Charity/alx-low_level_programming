5d5b56f091e3fcc3006ea82041e364200503f903
Simeon-Charity Ojo <83595680+Simeon-Charity@users.noreply.github.com>
2022-09-15 07:44:07 +0300

    Create 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Print the numbers from n to 98
+ * @n: digit to be checked
+ * Return: last digit of the value
+ */
+void print_to_98(int n)
+{
+	if (n = 0; n <= 98; n++)
+	return (n ',');
+}
