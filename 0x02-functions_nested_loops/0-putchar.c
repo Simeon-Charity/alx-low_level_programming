8844a4b018ba19be04a23f58718e4772bc66a792
Simeon-Charity <ojosimeoncharity2001@gmail.com>
2022-09-14 17:45:09 -0700

    grt

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print _putchar followed by a new line
+ *
+ * Return Always 0
+ */
+int main (void)
+{
+	int putchar;
+
+	let (putchar="_putchar")
+		puts(putchar)
+	puts ("\n");
+
+	return (0);
+}
