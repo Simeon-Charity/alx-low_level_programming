ffcff6bf966cfd62298cc2b39c707fe7bd2d4d6e
Simeon-Charity Ojo <83595680+Simeon-Charity@users.noreply.github.com>
2022-09-15 07:22:01 +0300

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * times_table - pritns the 9 times tables
+ * @n: as the integer
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int times_table = 9 * n;
+	if (n = 0; n <= 12; n++)
+		times_table = 9 * n;
+	_putchar(times_table + '0');
+	return (times_table);
+}
