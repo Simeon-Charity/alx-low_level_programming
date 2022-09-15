61971413083c0a68b60dbc04b88ecac6227d7e3f
Simeon-Charity Ojo <83595680+Simeon-Charity@users.noreply.github.com>
2022-09-15 06:43:24 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,6 @@
 /**
  * print_sign - print the sign of a number
  * @n: The character to be checked.
- *
  * Return: 1 and print + if n is greater than 0, 
  * return 0 if n is 0, return -1 is n is -
  */
