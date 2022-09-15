2c812e52cb3ef3fd0980d719a8620aa38ea47c19
Simeon-Charity Ojo <83595680+Simeon-Charity@users.noreply.github.com>
2022-09-15 06:48:22 +0300

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Compute the absolute value of an integer
+ * @c: The character to be checked.
+ *
+ * Return: ALways 0.
+ */
+int_abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
