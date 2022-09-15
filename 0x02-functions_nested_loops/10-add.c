28733d6e77da7c2af88afeaecc2f95933ca8adcf
Simeon-Charity Ojo <83595680+Simeon-Charity@users.noreply.github.com>
2022-09-15 07:33:39 +0300

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * int add - Print the addition of two number
+ * @a: The character to be checked.
+ * @b: The character to be checked.
+ * Return: last digit of the value
+ */
+int add(int a, int b)
+{
+	int num, a, b;
+	num = a + b;
+	return (num);
+}	
